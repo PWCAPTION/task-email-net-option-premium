13eeec19651a24b0142da75dc13aefd3ff0e2fd7
Jeff Gruenbaum <jgruenbaum@captionpartners.com>
2024-11-14 17:17:10 +0000

    pr review with Brayden

--- src/main.py
+++ src/main.py
@@ -1,26 +1,26 @@
+from caption_emailing import caption_accounts
 from caption_emailing.wrapper import email_on_failure
 from caption_ft_options_api.api_wrapper import FTOptionsAPI
-from caption_logger.logger import get_logger
 
 from settings import TASK_NAME
 from utils.email import generate_body_html, generate_email_subject, send_email
-from utils.ft_options import get_security_df_from_ft_options_api, remove_equity_securities
-from utils.utils_func import calc_option_premium, clean_mark_column, sum_long_option_premium, sum_option_premium, sum_short_option_premium
+from utils.ft_options import get_security_df_from_ft_options_api, remove_all_equities_from_positions_df
+from utils.utils_func import calc_option_premium
 
 
 @email_on_failure(TASK_NAME)
 def main():
+    df = get_security_df_from_ft_options_api(api=FTOptionsAPI.from_env())
+    df = remove_all_equities_from_positions_df(df)
+    df["Mark"] = df["Mark"].fillna(value=0)
+    df = calc_option_premium(df)
+    short_df = df[df["Quantity"] < 0]
+    short_option_premium = short_df["OptionPremium"].sum()
 
-    logger = get_logger()
-    logger.info(f"Starting {TASK_NAME}")
+    long_df = df[df["Quantity"] > 0]
+    long_option_premium = long_df["OptionPremium"].sum()
 
-    df_ft_options_api = get_security_df_from_ft_options_api(api=FTOptionsAPI.from_env())
-    df_ft_options_api_filtered = remove_equity_securities(df_ft_options_api, ["P1", "P2", "CHEF", "PH", "NHC", "NHC2"])
-    df_ft_options_api_filtered_cleaned = clean_mark_column(df_ft_options_api_filtered)
-    df_net_premium_calculated = calc_option_premium(df_ft_options_api_filtered_cleaned)
-    long_option_premium = sum_long_option_premium(df_net_premium_calculated)
-    short_option_premium = sum_short_option_premium(df_net_premium_calculated)
-    net_option_premium = sum_option_premium(df_net_premium_calculated)
+    net_option_premium = df["OptionPremium"].sum()
 
     print(long_option_premium)
     print(short_option_premium)
@@ -30,9 +30,20 @@ def main():
     subject = generate_email_subject()
     body_html = generate_body_html(long_option_premium, short_option_premium, net_option_premium)
 
-    send_email(subject, body_html)
-    logger.info("Net Option Premium email sent")
-    logger.info(f"Finished {TASK_NAME}")
+    send_email(
+        subject,
+        body_html,
+        [
+            # caption_accounts.TRADEOPS,
+            # caption_accounts.JEFFG,
+            # caption_accounts.BILLY,
+            caption_accounts.BRAYDEN,
+            # caption_accounts.DAN,
+            # caption_accounts.JASON,
+            # caption_accounts.JAZMIN,
+        ],
+    )
+    print("Net OptionPremium email sent")
 
 
 if __name__ == "__main__":
